d7c978cfa2d530cc9445be19b515eee316bb0001
Jack Snyder <3147104s@student.gla.ac.uk>
2026-03-04 10:36:15 +0000

    Copied over all files from my Rango project; Edited the settings.py and urls.py files

---
+++ WAD2_GroupProject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for WAD2_GroupProject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WAD2_GroupProject.settings')
+
+application = get_wsgi_application()
