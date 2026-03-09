42d22308662ec9871ec8f5a897589b94c70fcf5b
Tiyas Ghosh <3179859g@student.gla.ac.uk>
2026-03-09 20:50:20 +0000

    Created Comment and Tag models, modified all models to be consistent and linked in database, created timestamps for reviews and comments, allowed users to add bio to profile, linked comments and reviews to each profile, added comments where necessary!

--- gamecritique/models.py
+++ gamecritique/models.py
@@ -2,10 +2,31 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
-class Game(models.Model):
+# Each tag that can be assigned to each game - has a name. There is an N:N
+# relationship between tags and games.
+class Tag(models.Model):
+    MAX_TAG = 50
+
+    name = models.CharField(max_length=MAX_TAG, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Tag, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+# Each game in the database - has a name, a description, and
+# relevant category tags. Reviews and comments are linked to games.
+class Game(models.Model): 
     MAX_NAME = 128
+    MAX_DESC = 1000
+
+    tags = models.ManyToManyField(Tag, blank=True, related_name="games") # Tags for games
 
     name = models.CharField(max_length=MAX_NAME, unique=True)
+    description = models.TextField(max_length=MAX_DESC)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
@@ -15,20 +36,51 @@ class Game(models.Model):
     def __str__(self):
         return self.name
 
+# Each review in the database - has a maximum length of 1000, rating, and
+# a timestamp for creation. Reviews are linked to both a game and a user.
 class Review(models.Model):
-    MAX_TEXT = 1000
+    MAX_CONTENT = 1000
 
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    text = models.TextField(max_length=MAX_TEXT)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE) # Links review to game
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews") # Links review to user
+
+    content = models.TextField(max_length=MAX_CONTENT)
+    rating = models.PositiveSmallIntegerField(choices=[(i, i) for i in range(1, 6)]) # Rating on 1-5 scale
+    created_at_timestamp = models.DateTimeField(auto_now_add=True) # Timestamp of creation
+
+    class Meta:
+        verbose_name_plural = "Reviews"
 
     def __str__(self):
-        return self.text
+        return self.content
+
+# Each comment in the database - has a maximum length of 1000 and
+# a timestamp for creation. Comments are linked to both a game and a user.
+class Comment(models.Model):
+    MAX_CONTENT = 1000
     
-    
+    game = models.ForeignKey(Game, on_delete=models.CASCADE) # Links comment to game
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments") # Links comment to user
+
+    content = models.TextField(max_length=MAX_CONTENT)
+    created_at_timestamp = models.DateTimeField(auto_now_add=True) # Timestamp of creation
+
+    class Meta:
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return self.content
+
+# Each profile in the database, extends User model - has a profile picture
+# and a bio. Each user has one profile, and reviews and comments by this 
+# user can be found on their profile.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    MAX_BIO = 1000
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # 1:1 relationship with User model
 
     picture = models.ImageField(upload_to="profile_images", blank=True)
+    bio = models.TextField(max_length=MAX_BIO, blank=True)
 
     def __str__(self):
         return self.user.username
