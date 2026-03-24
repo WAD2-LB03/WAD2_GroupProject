ed4812b213a26d31ce175b1e94e4f701ee12a0c2
Tiyas Ghosh <3179859g@student.gla.ac.uk>
2026-03-24 01:39:02 +0000

    Created review.html to allow you to click on individual reviews and view comments.

--- gamecritique/urls.py
+++ gamecritique/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("game/<slug:game_name_slug>/", views.show_game, name="show_game"),
+    path("review/<int:review_id>/", views.show_review, name="show_review"),
     path("game/<slug:game_name_slug>/add_review/", views.add_review, name="add_review"),
     path("register/", views.register, name="register"),
     path("login/", views.user_login, name="login"),
--- gamecritique/views.py
+++ gamecritique/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 from datetime import datetime
 import random
 
 from gamecritique.models import Game, UserProfile
-from gamecritique.models import Review
+from gamecritique.models import Review, Comment
 from gamecritique.forms import ReviewForm, UserForm, UserProfileForm
 
 # Random game is chosen from database to be displayed on Index page each time opened
@@ -41,6 +42,14 @@ def show_game(request, game_name_slug):
         context_dict['reviews'] = None
 
     return render(request, 'gamecritique/game.html', context=context_dict)
+
+def show_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    comments = review.comments.all().order_by('-created_at_timestamp')
+
+    context = {'review': review, 'comments': comments,}
+
+    return render(request, 'gamecritique/review.html', context)
 
 @login_required
 def add_review(request, game_name_slug):
