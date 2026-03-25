da06ab05521d416f46c6add3d708c57ff374edd8
JackSnyder427 <3147104s@student.gla.ac.uk>
2026-03-25 15:21:58 +0000

    Fixed styling of Trending section on index page; Fixed horizontal scrolling; Enabled multiple horizontal scrollings on one page

--- static/scripts/interactivity/sidewaysScroll.js
+++ static/scripts/interactivity/sidewaysScroll.js
@@ -1,13 +1,37 @@
-const elements = document.getElementsByClassName("sideways-scroll");
+const scrollContainers = Array.from(document.getElementsByClassName("sideways-scroll"));
+const scrollDamp = .2;
 
-Array.from(elements).forEach((element) => {
-    element.addEventListener('wheel', (e) => {
+var scrollData = new Map();
+
+// Creates a map of data to handle scrolling animations for each horizontal scrolling element
+scrollContainers.forEach(container => {
+    scrollData.set(container, {
+        delta: 0,
+        isScrolling: false
+    });
+});
+
+scrollData.forEach((value, key) => {
+    key.addEventListener('wheel', (e) => {
         e.preventDefault();
 
-        element.scrollBy({
-            left: e.deltaY,
-            behavior: 'smooth'
-        });
-    },
-    { passive: false });
+        value.delta += e.deltaY;
+
+        if (!value.isScrolling) {
+            value.isScrolling = true;
+            requestAnimationFrame(() => step(value, key))
+        }
+    });
 });
+
+
+function step(value, key) {
+    if (Math.abs(value.delta) > .5) {
+        key.scrollLeft += value.delta * scrollDamp;
+        value.delta *= .8;
+        requestAnimationFrame(() => step(value, key));
+    } else {
+        value.delta = 0;
+        value.isScrolling = false;
+    }
+}
